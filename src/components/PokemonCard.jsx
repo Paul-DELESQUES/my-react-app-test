01f3ce05bc10b7acbd00f622f2f498166bd4119d
Paul-DELESQUES <pldelesques+github@gmail.com>
2023-10-09 21:14:42 +0200

    add quete 3 with 2 pokemons

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,34 @@
-function PokemonCard() {
+import React, { useState } from 'react';
+
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+  function PokemonCard() {
+    const [pokemon, setPokemon] = useState(pokemonList[0]);
+  
+    function changePokemon() {
+      setPokemon(pokemonList[1]);
+    }
+  
     return (
       <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-        <figcaption>Bulbasaur</figcaption>
+        {pokemon.imgSrc ? (
+          <img src={pokemon.imgSrc} alt={pokemon.name} />
+        ) : (
+          <p>???</p>
+        )}
+        <figcaption>{pokemon.name}</figcaption>
+        <button onClick={changePokemon}>Switch Pokemon</button>
       </figure>
     );
   }
-  
+   
   export default PokemonCard;
