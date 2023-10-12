ee2efa08c86894ae66500924721ddee6339d2036
Paul-DELESQUES <pldelesques+github@gmail.com>
2023-10-12 17:32:42 +0200

    debug

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import PokemonCard from "./components/PokemonCard.jsx"
 import React, { useState } from 'react';
-import PropTypes from 'prop-types'
-
-
+import PropTypes from 'prop-types';
+import PokemonCard from "./components/PokemonCard.jsx";
+import NavBar from './components/NavBar.jsx';
 
 const pokemonList = [
   {
@@ -11,24 +10,43 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
-    name: "mew", 
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
   },
-]
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 function App() {
-  const [thePokemon, setThePokemon] = useState(pokemonList[0]);
+  const [thePokemon, setThePokemon] = useState(0);
 
-  function changePokemon() {
-    const i = pokemonList.findIndex(e => e.name === thePokemon.name);
-    const j = (i + 1) % pokemonList.length;
-    
-    setThePokemon(pokemonList[j]);
-  }
+  // function changePokemon(e) {
+  //   if (e === "previous" && thePokemon > 0) {
+  //     setThePokemon(thePokemon - 1);
+  //   } else if (e === "next" && thePokemon < pokemonList.length - 1) {
+  //     setThePokemon(thePokemon + 1);
+  //   }
+  // }
 
   return (
     <div>
-      <PokemonCard pokemon={thePokemon} />
-      <button onClick={changePokemon}>Switch Pokemon</button>
+      <PokemonCard pokemon={pokemonList[thePokemon]} />
+      <NavBar
+         pokemonList={pokemonList}
+         setThePokemon={setThePokemon}
+      />
     </div>
   );
 }
@@ -36,9 +54,8 @@ function App() {
 App.propTypes = {
   pokemon: PropTypes.shape({
     name: PropTypes.string.isRequired,
-    img: PropTypes.string,
-  })
+    imgSrc: PropTypes.string,
+  }),
 };
-
 
 export default App;
